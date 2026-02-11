d5808b2f1b18358e79d50efd3382ef3476ae77ea
Jaimin Dhamecha <jaimin@softhealer.com>
2026-02-11 14:30:17 +0530

    [Add] added the code in the main branch as well

---
+++ custom_addons/partner_lang_risk/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Partner Language Risk",
+    "version": "19.0.1.0.0",
+    "category": "Contacts",
+    "summary": "Demo module with intentional review issues",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/partner_views.xml",
+    ],
+    "installable": True,
+}
